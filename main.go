9be6727d138e363095d032631ae4811788a023a1
Deepak Lamichhane <deepak.lamichhane@solarisbank.de>
2021-08-12 21:04:41 +0200

    add api keys with search form

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -21,14 +22,21 @@ func main() {
 
 	port := getEnv("PORT", "8080")
 
+	fs := http.FileServer(http.Dir("assets"))
+
 	router := http.NewServeMux()
+	router.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
 	router.HandleFunc("/", indexHandler)
 	router.HandleFunc("/articles", articlesHandler)
+	router.HandleFunc("/search", searchHandler)
+
 	err := http.ListenAndServe(":"+port, router)
 
 	if err != nil {
 		panic(err)
 	}
+
 }
 
 func getEnv(key, fallback string) string {
@@ -47,4 +55,23 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func articlesHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>I am in article page</h1>"))
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := r.URL.Parse(r.URL.String())
+	if err != nil {
+		fmt.Println("URL parse error")
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")
+
+	page := params.Get("page")
+	if page == "" {
+		x := "1"
+		page = x
+	}
+	fmt.Println("Your search query is: ", searchQuery)
+	fmt.Println("Your page is: ", page)
+
 }
